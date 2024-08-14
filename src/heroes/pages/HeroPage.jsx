8e7cb9d5ce3b8541fc782ba44e63ce849b211cac
Victor Jimenez <vjimenez@wigilabs.com>
2024-08-14 18:45:03 -0500

    primer comit

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -19,7 +19,7 @@ if(!hero){
     <div className="row mt-5">
       <div className="col-4">
         <img
-        src={`/assets/heroes/${id}.jpg`}
+        src={`/hero-app/assets/heroes/${id}.jpg`}
         alt={hero.superhero}
         className='img-thumbnail animate__animated animate__fadeInLeft' />
       </div>
